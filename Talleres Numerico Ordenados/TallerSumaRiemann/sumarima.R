6c4d771dcd8d7fbb3bf687a1f8f62837b22bc7fb
DanielaBeltranSaavedra <41376674+DanielaBeltranSaavedra@users.noreply.github.com>
2018-10-04 18:20:01 -0500

    trapecio

--- Talleres Numerico Ordenados/TallerSumaRiemann/sumarima.R
+++ Talleres Numerico Ordenados/TallerSumaRiemann/sumarima.R
@@ -1,15 +1,16 @@
 N <- 20;
 Nrange <- seq(5,N,by=1)
 area=rep(0,N)
-I_anal <- -0.2
+fun <- -0.2
 for(n in Nrange){
   x <- seq (-20,10,len=n)
   dx <- x[2]-x[1]
   f <- (1/(2*pi*n)^(1/2))*exp(((-1/2)*((x-3)^2))/(2*(n^2)))
   
-  area[n] <- dx*(sum(f[2:(n-1)])+(f[1]+f[n])/2)
+  area[n] <- dx*(sum(f[0:(n-1)])+(f[1]+f[n])/2)
+  print(area[n]-2)
 }
-err <- area-I_anal
+
 txt <- sprintf("el resultado de la suma de rima es =%5.3e para n=%d rectangles \n",area[n],n)
 cat(txt)
 par(mar=c(1,2,2,5))
